8c27bd043bcd55c9b763bcffe9f0279c3a3640ee
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-08-15 15:05:27 -0700

    Add files via upload

---
+++ KnightRider2/KnightRider2.ino
@@ -0,0 +1,64 @@
+void setup() {
+  //Knight Rider Lights #2
+  //Read more in the README
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(4,HIGH);
+  digitalWrite(13,HIGH);
+  delay(50);
+  digitalWrite(4,LOW);
+  digitalWrite(13,LOW);
+  
+  digitalWrite(5,HIGH);
+  digitalWrite(12,HIGH);
+  delay(50);
+  digitalWrite(5,LOW);
+  digitalWrite(12,LOW);
+  
+  digitalWrite(6,HIGH);
+  digitalWrite(11,HIGH);
+  delay(50);
+  digitalWrite(6,LOW);
+  digitalWrite(11,LOW);
+  
+  digitalWrite(7,HIGH);
+  digitalWrite(10,HIGH);
+  delay(50);
+  digitalWrite(7,LOW);
+  digitalWrite(10,LOW);
+  
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+  delay(50);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  
+  digitalWrite(7,HIGH);
+  digitalWrite(10,HIGH);
+  delay(50);
+  digitalWrite(7,LOW);
+  digitalWrite(10,LOW);
+  
+  digitalWrite(6,HIGH);
+  digitalWrite(11,HIGH);
+  delay(50);
+  digitalWrite(6,LOW);
+  digitalWrite(11,LOW);
+  
+  digitalWrite(5,HIGH);
+  digitalWrite(12,HIGH);
+  delay(50);
+  digitalWrite(5,LOW);
+  digitalWrite(12,LOW);
+}
